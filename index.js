2ad2c3759098aa7360d2294a6694ca31a9be686f
Paul Nechifor <paul@nechifor.net>
2016-01-08 22:56:14 +0000

    Correct retarted mistake.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var loaderUtils = require('loader-utils');
 
 module.exports = function (src) {
     var html = jade.render(src, {});
-    var template = _.template(src, loaderUtils.parseQuery(this.query));
+    var template = _.template(html, loaderUtils.parseQuery(this.query));
     return 'module.exports = ' + template;
 };
