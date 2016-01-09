62394e45dda95786ab9f5e67a3073b506e570f0c
Paul Nechifor <paul@nechifor.net>
2016-01-09 20:40:37 +0000

    Provide regexes that aren't modified when escaped.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var loaderUtils = require('loader-utils');
 
 module.exports = function (src) {
     var html = jade.render(src, {});
-    var template = _.template(html, loaderUtils.parseQuery(this.query));
+    var query = loaderUtils.parseQuery(this.query);
+    query = _.extend(query, {
+        interpolate: new RegExp(query.interpolate || '\\{\\{=(.+?)\\}\\}', 'g'),
+        escape: new RegExp(query.escape || '\\{\\{(.+?)\\}\\}', 'g'),
+        evaluate: new RegExp(query.evaluate || '[{<]%(.+?)%[>}]', 'g'),
+    });
+    var template = _.template(html, query);
     return 'module.exports = ' + template;
 };
